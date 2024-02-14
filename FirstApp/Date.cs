af2ce4861910fdc92b4834005e8d0e51f2ba64b9
Manuel Dominguez <dominguezguerrero1996@gmail.com>
2024-02-13 21:15:51 -0500

    first commir

---
+++ FirstApp/Date.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class Date
+    {
+        private int _year;
+        private int month;
+        private int day;
+
+        public Date(int year, int month, int day)
+        {
+            this._year = year;
+            this.month = month;
+            this.day = day;
+        }
+    }
+}
